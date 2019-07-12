45fdf71bab331f87798456c33b3e2d6f52edc38a
chris_fs_wang <f98989000@gmail.com>
2019-07-12 16:50:11 +0800

    Add User Model & Services

---
+++ GraphStudy/GraphStudy.Api/Schema/GraphStudySchema.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL;
+
+namespace GraphStudy.Api.Schema
+{
+    public class GraphStudySchema : GraphQL.Types.Schema
+    {
+        public GraphStudySchema(IDependencyResolver dependencyResolver)
+        {
+            DependencyResolver = dependencyResolver;
+        }
+    }
+}
---
+++ GraphStudy/GraphStudy.Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GraphStudy.Models;
+
+namespace GraphStudy.Services
+{
+    /// <summary>
+    /// 使用者Service
+    /// </summary>
+    public class UserService : IUserService
+    {
+        readonly List<User> users;
+
+        public UserService()
+        {
+            users = new List<User>();
+        }
+        public User GetUserById(int id)
+        {
+            User selectedUser = users.SingleOrDefault(context => context.Id == id);
+            if (selectedUser == null)
+            {
+                throw new ArgumentException(String.Format("User ID {0} 不存在", id));
+            }
+            return null;
+        }
+    }
+}
